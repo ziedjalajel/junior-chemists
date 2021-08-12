13d31cc51f7937fc3ec04035df4203adeafdd053
ziedjalajel <zied.jalajel1993@gmail.com>
2021-08-12 11:26:21 +0300

    enteruser

---
+++ src/components/Forms.js/UsernameForm.js
@@ -0,0 +1,11 @@
+const UsernameForm = () => {
+  return (
+    <div className="usernameform">
+      <div className="left-div">
+        <h1>Enter your Name :</h1>
+      </div>
+      <input className="left-input" type="text" placeholder="..." />
+    </div>
+  );
+};
+export default UsernameForm;
