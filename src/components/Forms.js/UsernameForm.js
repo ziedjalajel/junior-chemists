b9669eef75507dbb4d56c827604dc403dab6c916
Ayah-AQ <ayoya_1998@hotmail.com>
2021-08-12 14:18:24 +0300

    ..

--- src/components/Forms.js/UsernameForm.js
+++ src/components/Forms.js/UsernameForm.js
@@ -1,11 +1,15 @@
+import "./Form.css"
 const UsernameForm = () => {
-  return (
+  return ( 
+    <body>
     <div className="usernameform">
       <div className="left-div">
         <h1>Enter your Name :</h1>
       </div>
       <input className="left-input" type="text" placeholder="..." />
+      
     </div>
+    </body>
   );
 };
 export default UsernameForm;
