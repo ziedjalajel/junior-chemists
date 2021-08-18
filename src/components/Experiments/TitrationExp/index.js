f6c69b504bdae204ec944b6f6a18102f54418d35
ziedjalajel <zied.jalajel1993@gmail.com>
2021-08-18 18:09:33 +0300

    exp 1 ready

--- src/components/Experiments/TitrationExp/index.js
+++ src/components/Experiments/TitrationExp/index.js
@@ -21,43 +21,50 @@ import dropb1 from "./images/drop.png";
 import redcolor from "./images/flask_liquid_me.png";
 import pinkcolor from "./images/colorchaangeafterph.png";
 import dropb2 from "./images/drop.png";
+//components
+import Procedure from "./Procedure";
 
 const Titration = () => {
   return (
-    <div className="Exp1">
-      <img src={burette_stand} alt="" className="burette_stand" />
-      <img src={flask} alt="" className="flask" />
-      <img src={flask_liquid} alt="" className="f-l" />
-      <div>
-        <img src={pp} alt="" className="pp" />
-        <h6 className="pph">Phenophthalene</h6>
+    <>
+      <div className="Exp1">
+        <img src={burette_stand} alt="" className="burette_stand" />
+        <img src={flask} alt="" className="flask" />
+        <img src={flask_liquid} alt="" className="f-l" />
+        <div>
+          <img src={pp} alt="" className="pp" />
+          <h5 className="pph">Phenophthalene</h5>
+        </div>
+        <div>
+          <img src={mo} alt="" className="mo" />
+          <h5 className="moh">Methyl Orange</h5>
+        </div>
+        <img src={p} alt="" className="p" />
+        <img src={m} alt="" className="m" />
+        <img src={dropperp} alt="" className="dropperp" />
+        <img src={dropperp} alt="" className="dropperp2" />
+        <img src={dropperm} alt="" className="dropperm" />
+        <img src={dropperm} alt="" className="dropperm2" />
+        <img src={panadol} alt="" className="panadol" />
+        <img src={droppermm} alt="" className="droppermm" />
+        <img src={droppermm} alt="" className="droppermm2" />
+        <img src={dropperpp} alt="" className="dropperpp" />
+        <img src={dropperpp} alt="" className="dropperpp2" />
+        <img src={dropm} alt="" className="dropm" />
+        <img src={dropp} alt="" className="dropp" />
+        <img src={mpanadol} alt="" className="mpanadol" />
+        <div className="burettediv"> </div>
+        <img src={burette} alt="" className="burette" />
+        <img src={orangecolor} alt="" className="orangecolor" />
+        <img src={dropb1} alt="" className="dropb1" />
+        <img src={redcolor} alt="" className="redcolor" />
+        <img src={pinkcolor} alt="" className="pinkcolor" />
+        <img src={dropb2} alt="" className="dropb2" />
       </div>
       <div>
-        <img src={mo} alt="" className="mo" />
-        <h6 className="moh">Methyl Orange</h6>
+        <Procedure />
       </div>
-      <img src={p} alt="" className="p" />
-      <img src={m} alt="" className="m" />
-      <img src={dropperp} alt="" className="dropperp" />
-      <img src={dropperp} alt="" className="dropperp2" />
-      <img src={dropperm} alt="" className="dropperm" />
-      <img src={dropperm} alt="" className="dropperm2" />
-      <img src={panadol} alt="" className="panadol" />
-      <img src={droppermm} alt="" className="droppermm" />
-      <img src={droppermm} alt="" className="droppermm2" />
-      <img src={dropperpp} alt="" className="dropperpp" />
-      <img src={dropperpp} alt="" className="dropperpp2" />
-      <img src={dropm} alt="" className="dropm" />
-      <img src={dropp} alt="" className="dropp" />
-      <img src={mpanadol} alt="" className="mpanadol" />
-      <div className="burettediv"> </div>
-      <img src={burette} alt="" className="burette" />
-      <img src={orangecolor} alt="" className="orangecolor" />
-      <img src={dropb1} alt="" className="dropb1" />
-      <img src={redcolor} alt="" className="redcolor" />
-      <img src={pinkcolor} alt="" className="pinkcolor" />
-      <img src={dropb2} alt="" className="dropb2" />
-    </div>
+    </>
   );
 };
 export default Titration;
