c00553c77887db5bc5095c7966c1d9c8ee51be07
ziedjalajel <zied.jalajel1993@gmail.com>
2021-08-19 23:42:52 +0300

    exp 3 done

--- src/components/Experiments/SoapExp/Procedure.js
+++ src/components/Experiments/SoapExp/Procedure.js
@@ -1,28 +1,59 @@
+//styling
+import "./soapProc.css";
+
 const SoapProcedure = () => {
   return (
-    <div className="soapProc">
-      <p className="introSoap">Soap Procedure :</p>
-      <p className="Sproc1">
-        1. Slowly Pour Sodium Hydroxiod ( NaOH ) into water <br /> NOTE : be
-        sure never do the opposite in ur life.
-      </p>
-      <p className="Sproc2">
-        2. Allow it to sit, it will become hot due to Chemical reaction.
-      </p>
-      <p className="Sproc3">3. Heat the oil.</p>
-      <p className="Sproc4">
-        4. When both oil and NaOH mix is 37 ° C pour the oil into the mix Powel.
-      </p>
-      <p className="Sproc5">
-        4. Let the solution out of the burette until the indicator changes
-        color, and record the value on the burette.
-      </p>
-      <p className="Sproc6">Now let's move to the phenophthalene indicator.</p>
-      <p className="Sproc7">
-        -Phenophthalene reacts with base and gives pink color while in acid the
-        color vanishes-
-      </p>
-    </div>
+    <>
+      <div>
+        <p className="introSoap">Soap Procedure :</p>
+        <p className="Sproc1">
+          1. Slowly Pour Sodium Hydroxiod ( NaOH ) into water <br /> NOTE : Be
+          sure never do the opposite in your life.
+        </p>
+        <p className="Sproc2">
+          2. Allow it to sit, It will become hot due to Chemical reaction.
+        </p>
+        <p className="Sproc3">3. Heat the oil.</p>
+        <p className="Sproc4">
+          4. When both oil and NaOH mix heat is 37° C pour the oil into the mix
+          bowl.
+        </p>
+        <p className="Sproc5">5. Slowly add NaOH.</p>
+        <p className="Sproc6">6. Stir until all mix.</p>
+        <p className="Sproc7">7. Now mix it using spatula for 5 minutes.</p>
+        <p className="Sproc8">
+          8. When it's thick enough pour the mixture into the soap mold.
+        </p>
+        <p className="Sproc9">9. Cover it with plastic wrap.</p>
+        <p className="Sproc10">
+          10. It need 24 to 48 hours to become soap but need at least 4 weeks
+          before use.
+        </p>
+      </div>
+      <div className="soapProc">
+        <p>Soap Procedure :</p>
+
+        <p>
+          1. Slowly Pour Sodium Hydroxiod ( NaOH ) into water <br /> NOTE : Be
+          sure never do the opposite in your life.
+        </p>
+        <p>2. Allow it to sit, It will become hot due to Chemical reaction.</p>
+        <p>3. Heat the oil.</p>
+        <p>
+          4. When both oil and NaOH mix heat is 37° C pour the oil into the mix
+          bowl.
+        </p>
+        <p>5. Slowly add NaOH.</p>
+        <p>6. Stir until all mix.</p>
+        <p>7. Now mix it using spatula for 5 minutes.</p>
+        <p>8. When it's thick enough pour the mixture into the soap mold.</p>
+        <p>9. Cover it with plastic wrap.</p>
+        <p>
+          10. It need 24 to 48 hours to become soap but need at least 4 weeks
+          before use.
+        </p>
+      </div>
+    </>
   );
 };
 export default SoapProcedure;
--- src/components/Experiments/SoapExp/index.js
+++ src/components/Experiments/SoapExp/index.js
@@ -31,45 +31,62 @@ import soapdrop1 from "./images/soapdrop.png";
 import soapdrop2 from "./images/soapdrop.png";
 import soapdrop3 from "./images/soapdrop.png";
 import soapdrop4 from "./images/soapdrop.png";
+import soapInMold from "./images/soapInMold.png";
+import soap from "./images/soap.png";
+import glassRod from "./images/glassRod.png";
+//component
+import SoapProcedure from "./Procedure";
 
 const Soap = () => {
+  function refreshPage() {
+    window.location.reload(false);
+  }
   return (
-    <div className="Exp3">
-      <img src={Spoon} alt="" className="Spoon" />
-      <img src={bowl} alt="" className="bowl" />
-      <img src={oliveoil} alt="" className="oliveoil" />
-      <img src={oliveoilreturn} alt="" className="oliveoilreturn" />
-      <img src={soapmold} alt="" className="soapmold" />
-      <img src={thermo} alt="" className="thermo" />
-      <img src={water} alt="" className="water" />
-      <img src={oildrop} alt="" className="oildrop" />
-      <img src={Naoh} alt="" className="Naoh" />
-      <img src={Naoh2} alt="" className="Naoh2" />
-      <img src={bowloil} alt="" className="bowloil" />
-      <img src={oilnaoh} alt="" className="oilnaoh" />
-      <img src={oven} alt="" className="oven" />
-      <img src={beaker} alt="" className="beaker" />
-      <img src={naohdrop} alt="" className="naohdrop" />
-      <img src={Naohrotated} alt="" className="Naohrotated" />
-      <img src={oildropBurner} alt="" className="oildropBurner" />
-      <img src={thermoInBurner} alt="" className="thermoInBurner" />
-      <img src={oilOnBeaker} alt="" className="oilOnBeaker" />
-      <img src={yellowbeaker} alt="" className="yellowbeaker" />
-      <img src={ybRotated} alt="" className="ybRotated" />
-      <img src={naohbrotated} alt="" className="naohbrotated" />
-      <img src={bluedrop} alt="" className="bluedrop" />
-      <img src={poursoap1} alt="" className="poursoap1" />
-      <img src={poursoap2} alt="" className="poursoap2" />
-      <img src={poursoap3} alt="" className="poursoap3" />
-      <img src={poursoap4} alt="" className="poursoap4" />
-      <img src={soapdrop1} alt="" className="soapdrop1" />
-      <img src={soapdrop2} alt="" className="soapdrop2" />
-      <img src={soapdrop3} alt="" className="soapdrop3" />
-      <img src={soapdrop4} alt="" className="soapdrop4" />
-      <div className="thermodiv"></div>
-      <div className="naohdiv"></div>
-      <div className="increseHeatNaoh"></div>
-    </div>
+    <>
+      <button className="btn" onClick={refreshPage}>
+        <p className="Repeat">Repeat</p>
+      </button>
+      <div className="Exp3">
+        <img src={Spoon} alt="" className="Spoon" />
+        <img src={bowl} alt="" className="bowl" />
+        <img src={oliveoil} alt="" className="oliveoil" />
+        <img src={oliveoilreturn} alt="" className="oliveoilreturn" />
+        <img src={soapmold} alt="" className="soapmold" />
+        <img src={thermo} alt="" className="thermo" />
+        <img src={water} alt="" className="water" />
+        <img src={oildrop} alt="" className="oildrop" />
+        <img src={Naoh} alt="" className="Naoh" />
+        <img src={Naoh2} alt="" className="Naoh2" />
+        <img src={bowloil} alt="" className="bowloil" />
+        <img src={oilnaoh} alt="" className="oilnaoh" />
+        <img src={oven} alt="" className="oven" />
+        <img src={beaker} alt="" className="beaker" />
+        <img src={naohdrop} alt="" className="naohdrop" />
+        <img src={Naohrotated} alt="" className="Naohrotated" />
+        <img src={oildropBurner} alt="" className="oildropBurner" />
+        <img src={thermoInBurner} alt="" className="thermoInBurner" />
+        <img src={oilOnBeaker} alt="" className="oilOnBeaker" />
+        <img src={yellowbeaker} alt="" className="yellowbeaker" />
+        <img src={ybRotated} alt="" className="ybRotated" />
+        <img src={naohbrotated} alt="" className="naohbrotated" />
+        <img src={bluedrop} alt="" className="bluedrop" />
+        <img src={poursoap1} alt="" className="poursoap1" />
+        <img src={poursoap2} alt="" className="poursoap2" />
+        <img src={poursoap3} alt="" className="poursoap3" />
+        <img src={poursoap4} alt="" className="poursoap4" />
+        <img src={soapdrop1} alt="" className="soapdrop1" />
+        <img src={soapdrop2} alt="" className="soapdrop2" />
+        <img src={soapdrop3} alt="" className="soapdrop3" />
+        <img src={soapdrop4} alt="" className="soapdrop4" />
+        <img src={soapInMold} alt="" className="soapInMold" />
+        <img src={soap} alt="" className="soap" />
+        <img src={glassRod} alt="" className="glassRod" />
+        <div className="thermodiv"></div>
+        <div className="naohdiv"></div>
+        <div className="increseHeatNaoh"></div>
+      </div>
+      <SoapProcedure />
+    </>
   );
 };
 export default Soap;
