6881d9b8119c7bc47b1335794adc0c1ef86a06e2
ziedjalajel <zied.jalajel1993@gmail.com>
2021-08-20 03:54:24 +0300

    ayaaaaa

--- src/components/Experiments/PickExp.js
+++ src/components/Experiments/PickExp.js
@@ -1,9 +1,7 @@
 import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 //styling
 import "./Exp.css";
-// import E1 from "../../images/exp1.png";
-// import E2 from "../../images/exp2.png";
-// import E3 from "../../images/exp3.png";
 import cbook1 from "../../images/cBook1.png";
 import titrationBook from "../../images/titrationBook.gif";
 import warning from "../../images/warning.png";
@@ -12,60 +10,69 @@ import soapE from "../../images/soapE.png";
 import soapExp from "../../images/soapExp.gif";
 
 const PickExp = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+  const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+  const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
   return (
     <>
-      <div className="pickexp">
-        <div className="house-lists">
-          <Link
-            to="/titration"
-            style={{ textDecoration: "none", color: "white" }}
-          >
-            <div
-              className="house"
-              style={{ backgroundImage: `url(${titrationBook})` }}
+      {isDesktopOrLaptop && (
+        <div className="pickexp">
+          <div className="house-lists">
+            <Link
+              to="/titration"
+              style={{ textDecoration: "none", color: "white" }}
             >
-              <div className="house-inner">
-                <div className="sigil">
-                  <img src={cbook1} alt="" />
+              <div
+                className="house"
+                style={{ backgroundImage: `url(${titrationBook})` }}
+              >
+                <div className="house-inner">
+                  <div className="sigil">
+                    <img src={cbook1} alt="" />
+                  </div>
+                  <div className="house-name">Titration Exp.</div>
                 </div>
-                <div className="house-name">Titration Exp.</div>
               </div>
-            </div>
-          </Link>
-          <Link
-            to="/sodium-exp"
-            style={{ textDecoration: "none", color: "white" }}
-          >
-            <div
-              className="house"
-              style={{ backgroundImage: `url(${warningExp})` }}
+            </Link>
+            <Link
+              to="/sodium-exp"
+              style={{ textDecoration: "none", color: "white" }}
             >
-              <div className="house-inner">
-                <div className="sigil">
-                  <img src={warning} alt="" />
+              <div
+                className="house"
+                style={{ backgroundImage: `url(${warningExp})` }}
+              >
+                <div className="house-inner">
+                  <div className="sigil">
+                    <img src={warning} alt="" />
+                  </div>
+                  <div className="house-name">Warning Exp.</div>
                 </div>
-                <div className="house-name">Warning Exp.</div>
               </div>
-            </div>
-          </Link>
-          <Link
-            to="/soap-exp"
-            style={{ textDecoration: "none", color: "white" }}
-          >
-            <div
-              className="house"
-              style={{ backgroundImage: `url(${soapExp})` }}
+            </Link>
+            <Link
+              to="/soap-exp"
+              style={{ textDecoration: "none", color: "white" }}
             >
-              <div className="house-inner">
-                <div className="sigil">
-                  <img src={soapE} alt="" />
+              <div
+                className="house"
+                style={{ backgroundImage: `url(${soapExp})` }}
+              >
+                <div className="house-inner">
+                  <div className="sigil">
+                    <img src={soapE} alt="" />
+                  </div>
+                  <div className="house-name">Soap Exp.</div>
                 </div>
-                <div className="house-name">Soap Exp.</div>
               </div>
-            </div>
-          </Link>
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
