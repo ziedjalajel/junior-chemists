8d19a648189b8c734dd30fbe3c8fbff498cbf5f4
ziedjalajel <zied.jalajel1993@gmail.com>
2021-09-03 23:16:57 +0300

    still deploying

--- src/store/actions/instance.js
+++ src/store/actions/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://junior-chemists-be-s2zll.ondigitalocean.app/",
 });
 
 export default instance;
